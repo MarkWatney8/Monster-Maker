2992246a9c50390eb181877ad89abd3efa9292f9
Christopher Long <christopherlong23@gmail.com>
2022-10-06 20:01:30 -0600

    added csv file writer and csv file

--- class_to_csv.py
+++ class_to_csv.py
@@ -1,3 +1,13 @@
-import csv
+from csv import writer
+
+
+def to_file(file="monsters.csv", object=None):
+    with open(file, "a", newline='\n') as f:
+        if object:
+            data = vars(object).values()
+            writer = writer(f)
+            writer.writerow(data)
+        else:
+            print("No object!")
 
 
