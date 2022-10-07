ad816f9c52d01e58a80fe0bd7c8376f63e954125
Christopher Long <christopherlong23@gmail.com>
2022-10-06 20:50:17 -0600

    started setting up main python file and input

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello Shane!")
+import monster
+import class_to_csv
+
+
+test = input("This is a test input. ")
+
+print(test)
+
+
+monster_type = input("""
+1 - Random Monster
+2 - Custom Monster""")
+
+if(int(monster_type) == 1):
+    print("Random Monster")
+else:
+    print("Custom Monster")
