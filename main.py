4b3aa71b2f2c06bff2758c1d50b752232bd4de97
PopBro1 <popcorn2580@gmail.com>
2022-10-06 21:49:24 -0600

    done?

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from monster import Monster
 from class_to_csv import to_file
 
 
-
-m = Monster()
-to_file(object=m)
-
-test = input("This is a test input. ")
-
-print(test)
-
+def create_custom_monster() -> Monster :
+    monster_name    = input("\nWhat name do you want it to have?\n")
+    monster_health  = input("\nWhat is the health of the creature?\n")
+    monster_ac      = input("\nWhat is the ac of the creature?\n")
+    monster_damage  = input("\nWhat is the damage of the creature?\n")
+    monster_speed   = input("\nWhat is the speed of the creature?\n")
+    
+    return Monster(monster_name, monster_health, monster_ac, monster_damage, monster_speed)
+    
 
 monster_type = input("""
 1 - Random Monster
-2 - Custom Monster""")
+2 - Custom Monster
+                     """)
 
 if(int(monster_type) == 1):
     print("Random Monster")
+    m = Monster()
+    to_file(object=m)
 else:
     print("Custom Monster")
+    to_file(object = create_custom_monster())
+
+
