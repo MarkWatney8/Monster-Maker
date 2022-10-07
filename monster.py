cccb828318aed7d667a8b68e17a0ffed2aaa8bab
PopBro1 <popcorn2580@gmail.com>
2022-10-06 20:11:00 -0600

    random monster stats

---
+++ monster.py
@@ -0,0 +1,36 @@
+import random
+
+
+class Monster:
+
+
+    def __init__(self, name="Gourge", health=15, ac=12, damage=8, speed=30):
+        self.name = name
+        self.health = health
+        self.ac = ac
+        self.damage = damage
+        self.speed = speed
+
+    
+    def generate_random_health(self):
+        health = random.randrange(1, 81)
+        self.health = health
+
+
+    def generate_random_ac(self):
+        ac = random.randrange(10, 25)
+        self.ac = ac
+
+
+    def generate_random_damage(self):
+        damage = random.randrange(2, 20, 2)
+        self.damage = damage
+
+
+    def generate_random_speed(self):
+        speed = random.randrange(10, 50, 10)
+        self.speed = speed
+
+
+
+
