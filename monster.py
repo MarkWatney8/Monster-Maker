14de0303f2dcce1058da197b054d7829fab88ffa
PopBro1 <popcorn2580@gmail.com>
2022-10-06 21:12:23 -0600

    reorganized monster

--- monster.py
+++ monster.py
@@ -1,35 +1,38 @@
-import random
+from random import randrange
 
 
 class Monster:
 
-
-    def __init__(self, name="Gourge", health=random.randrange(1, 81), ac=random.randrange(10, 25), damage=random.randrange(2, 20, 2), speed=random.randrange(10, 50, 10)):
-        self.name = name
+    def __init__(   
+                    self,
+                    name   = "Gourge", 
+                    health = randrange(1, 81),
+                    ac     = randrange(10, 25),
+                    damage = randrange(2, 20, 2),
+                    speed  = randrange(10, 50, 10)
+                ):
+        
+        self.name   = name
         self.health = health
-        self.ac = ac
+        self.ac     = ac
         self.damage = damage
-        self.speed = speed
+        self.speed  = speed
 
     
     def generate_random_health(self):
-        health = random.randrange(1, 81)
-        self.health = health
+        self.health = randrange(1, 81)
 
 
     def generate_random_ac(self):
-        ac = random.randrange(10, 25)
-        self.ac = ac
+        self.ac = randrange(10, 25)
 
 
     def generate_random_damage(self):
-        damage = random.randrange(2, 20, 2)
-        self.damage = damage
+        self.damage = randrange(2, 20, 2)
 
 
     def generate_random_speed(self):
-        speed = random.randrange(10, 50, 10)
-        self.speed = speed
+        self.speed = randrange(10, 50, 10)
 
 
 
